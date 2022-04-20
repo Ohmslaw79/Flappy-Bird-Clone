ae6dbb291afad148cbc621f6c3d7ea74033c7cb9
Noah Elder <noahtheswimmer79@gmail.com>
2022-04-20 19:49:47 -0400

    Project Main created

---
+++ Flappy_Bord/src/main.c
@@ -0,0 +1,251 @@
+#include "stm32f0xx.h"
+#include "lcd.h"
+#include <stdint.h>
+#include "midi.h"
+#include "midiplay.h"
+
+// The number of simultaneous voices to support.
+#define VOICES 15
+
+// An array of "voices".  Each voice can be used to play a different note.
+// Each voice can be associated with a channel (explained later).
+// Each voice has a step size and an offset into the wave table.
+struct {
+    uint8_t in_use;
+    uint8_t note;
+    uint8_t chan;
+    uint8_t volume;
+    int     step;
+    int     offset;
+} voice[VOICES];
+
+void init_lcd_spi(void)
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+
+	//set as outputs
+	GPIOB->MODER &= ~0x30c30000; //0011 0000 1100 0011 0000 0000 0000 0000
+	GPIOB->MODER |= 0x10410000;  //0001 0000 0100 0001 0000 0000 0000 0000
+
+	//set logic high
+	GPIOB->ODR |= 0x4900; //0100 1001 0000 0000
+
+	//set to alternative function 0
+	GPIOB->AFR[0] &= ~0xf0f000 ;//1111 0000 1111 0000 0000 0000
+
+	//"""Enable the SPI channel."""
+	RCC->APB1ENR |= 1<<14;
+
+	//"""Ensure that the CR1_SPE bit is clear. Many of the bits set in the control registers require that the SPI channel is not enabled."""
+	SPI2->CR1 &= ~(1<<6);
+
+	//"""Configure the interface for a 8-bit word size."""
+	SPI2->CR2 = SPI_CR2_DS_0 |SPI_CR2_DS_1 |SPI_CR2_DS_2;
+
+	//"""Configure the SPI channel to be in master mode."""
+	SPI2->CR1 |= (1<<2);
+
+	//Set the SSM and SSI bits
+	SPI2->CR1 |= SPI_CR1_SSI | SPI_CR1_SSM;
+
+	SPI2->CR1 |= (1<<6);
+
+}
+
+void setup_buttons(void)
+{
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+
+    //set as outputs
+    GPIOC->MODER &= ~0xff00; //1111 1111 0000 0000
+    GPIOC->MODER |= 0x5500;//0101 0101 0000 0000
+
+    //set open drain
+    GPIOC->OTYPER |= 0xf0;//1111 0000
+
+    //set as inputs
+    GPIOC->MODER &= ~0xff; //1111 1111
+
+    //enable internal pullup resistors
+    GPIOC->PUPDR &= ~0xff;
+    GPIOC->PUPDR |= 0x55;
+
+}
+
+void basic_drawing(void);
+void move_ball(void);
+
+// We'll use the Timer 6 IRQ to recompute samples and feed those
+// samples into the DAC.
+void TIM6_DAC_IRQHandler(void)
+{
+    // DONE: Remember to acknowledge the interrupt right here.
+    TIM6->SR &= ~TIM_SR_UIF;
+    int sample = 0;
+    for(int x=0; x < sizeof voice / sizeof voice[0]; x++) {
+        if (voice[x].in_use) {
+            voice[x].offset += voice[x].step;
+            if (voice[x].offset >= N<<16)
+                voice[x].offset -= N<<16;
+            sample += (wavetable[voice[x].offset>>16] * voice[x].volume) >> 4;
+        }
+    }
+    sample = (sample >> 10) + 2048;
+    if (sample > 4095)
+        sample = 4095;
+    else if (sample < 0)
+        sample = 0;
+    DAC->DHR12R1 = sample;
+}
+
+// Initialize the DAC so that it can output analog samples
+// on PA4.  Configure it to be triggered by TIM6 TRGO.
+void init_dac(void)
+{
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN;
+	DAC->CR &= ~(DAC_CR_TSEL1);
+	DAC->CR |= DAC_CR_TEN1;
+	DAC->CR |= DAC_CR_EN1;
+    DMA1_Channel5->CNDTR = 23;
+}
+
+// Initialize Timer 6 so that it calls TIM6_DAC_IRQHandler
+// at exactly RATE times per second.  You'll need to select
+// a PSC value and then do some math on the system clock rate
+// to determine the value to set for ARR.  Set it to trigger
+// the DAC by enabling the Update Trigger in the CR2 MMS field.
+void init_tim6(void)
+{
+    RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+    TIM6->PSC = (48000000 / (2 * RATE)) - 1;
+    TIM6->ARR = 1;
+
+    TIM6->DIER |= TIM_DIER_UIE;
+    NVIC->ISER[0] |= 1 << 17;
+    TIM6->CR2 &= ~TIM_CR2_MMS;
+    TIM6->CR2 |= TIM_CR2_MMS_1;
+
+    TIM6->CR1 |= TIM_CR1_CEN;
+
+}
+
+// Find the voice current playing a note, and turn it off.
+void note_off(int time, int chan, int key, int velo)
+{
+    int n;
+    for(n=0; n<sizeof voice / sizeof voice[0]; n++) {
+        if (voice[n].in_use && voice[n].note == key) {
+            voice[n].in_use = 0; // disable it first...
+            voice[n].chan = 0;   // ...then clear its values
+            voice[n].note = key;
+            voice[n].step = step[key];
+            return;
+        }
+    }
+}
+
+// Find an unused voice, and use it to play a note.
+void note_on(int time, int chan, int key, int velo)
+{
+    if (velo == 0) {
+        note_off(time, chan, key, velo);
+        return;
+    }
+    int n;
+    for(n=0; n<sizeof voice / sizeof voice[0]; n++) {
+        if (voice[n].in_use == 0) {
+            voice[n].note = key;
+            voice[n].step = step[key];
+            voice[n].offset = 0;
+            voice[n].volume = velo;
+            voice[n].chan = chan;
+            voice[n].in_use = 1;
+            return;
+        }
+    }
+}
+
+void set_tempo(int time, int value, const MIDI_Header *hdr)
+{
+    // This assumes that the TIM2 prescaler divides by 48.
+    // It sets the timer to produce an interrupt every N
+    // microseconds, where N is the new tempo (value) divided by
+    // the number of divisions per beat specified in the MIDI file header.
+    TIM2->ARR = value/hdr->divisions - 1;
+}
+
+const float pitch_array[] = {
+0.943874, 0.945580, 0.947288, 0.948999, 0.950714, 0.952432, 0.954152, 0.955876,
+0.957603, 0.959333, 0.961067, 0.962803, 0.964542, 0.966285, 0.968031, 0.969780,
+0.971532, 0.973287, 0.975046, 0.976807, 0.978572, 0.980340, 0.982111, 0.983886,
+0.985663, 0.987444, 0.989228, 0.991015, 0.992806, 0.994599, 0.996396, 0.998197,
+1.000000, 1.001807, 1.003617, 1.005430, 1.007246, 1.009066, 1.010889, 1.012716,
+1.014545, 1.016378, 1.018215, 1.020054, 1.021897, 1.023743, 1.025593, 1.027446,
+1.029302, 1.031162, 1.033025, 1.034891, 1.036761, 1.038634, 1.040511, 1.042390,
+1.044274, 1.046160, 1.048051, 1.049944, 1.051841, 1.053741, 1.055645, 1.057552,
+};
+
+void pitch_wheel_change(int time, int chan, int value)
+{
+    //float multiplier = pow(STEP1, (value - 8192.0) / 8192.0);
+    float multiplier = pitch_array[value >> 8];
+    for(int n=0; n<sizeof voice / sizeof voice[0]; n++) {
+        if (voice[n].in_use && voice[n].chan == chan) {
+            voice[n].step = step[voice[n].note] * multiplier;
+        }
+    }
+}
+
+uint8_t notes[] = { 60,62,64,65,67,69,71,72,71,69,67,65,64,62,60,0 };
+uint8_t num = sizeof notes / sizeof notes[0] - 1;
+void TIM2_IRQHandler(void)
+{
+    TIM2->SR &= ~TIM_SR_UIF;    
+
+    midi_play();
+}
+
+// Configure timer 2 so that it invokes the Update interrupt
+// every n microseconds.  To do so, set the prescaler to divide
+// by 48.  Then the CNT will count microseconds up to the ARR value.
+// Basically ARR = n-1
+// Set the ARPE bit in the CR1 so that the timer waits until the next
+// update before changing the effective ARR value.
+// Call NVIC_SetPriority() to set a low priority for Timer 2 interrupt.
+// See the lab 6 text to understand how to do so.
+void init_tim2(int n) {
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    TIM2->PSC = 47;
+    TIM2->ARR = n-1;
+
+    TIM2->DIER |= TIM_DIER_UIE;
+    NVIC->ISER[0] |= 1 << 15;
+    TIM2->CR1 |= TIM_CR1_ARPE;
+
+    //NVIC_SetPriority(TIM2_IRQn, 3);
+
+    TIM2->CR1 |= TIM_CR1_CEN;
+}
+
+int main(void)
+{
+    setup_buttons();
+    LCD_Setup(); // this will call init_lcd_spi()
+    basic_drawing();
+    move_ball();
+
+    init_wavetable_hybrid2();
+    init_dac();
+    init_tim6();
+    MIDI_Player *mp = midi_init(midifile);
+    // The default rate for a MIDI file is 2 beats per second
+    // with 48 ticks per beat.  That's 500000/48 microseconds.
+    init_tim2(10417);
+    for(;;) {
+        asm("wfi");
+        // If we hit the end of the MIDI file, start over.
+        if (mp->nexttick == MAXTICKS)
+            mp = midi_init(midifile);
+    }
+}
+
